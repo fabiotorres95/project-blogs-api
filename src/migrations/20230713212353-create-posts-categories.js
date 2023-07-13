1f0b5bd31415f1a93441c73541fe9f216b5b7b6f
fabiotorres95 <engfabiotorres@outlook.com>
2023-07-13 18:39:28 -0300

    created categories and posts_categories migrations

---
+++ src/migrations/20230713212353-create-posts-categories.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', { 
+      postId: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'post_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'blog_posts',
+          key: 'id'
+        }
+      },
+      categoryId: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'category_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'categories',
+          key: 'id'
+        }
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+  }
+};
